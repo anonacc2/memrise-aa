4a2876339e6fe440cdaba5733f36c892c5d87d9d
anonacc2 <102777099+anonacc2@users.noreply.github.com>
2022-04-21 20:40:01 -0600

    Update train-transd.js

--- train-transd.js
+++ train-transd.js
@@ -3,6 +3,7 @@ let tran = {}
 datas.forEach((e)=>{
     if (e.innerText.split("\n").length>=3) {
         let a = e.innerText.split("\n").splice(1,1e9)
-        tran[a[1]] = a[0]
+        tran[a[1]] = a[0] 
     }
 })
+document.documentElement.innerText=JSON.stringify(tran)
